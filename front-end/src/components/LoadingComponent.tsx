ff914c02693c7f39f5eeec419085df1dff70b153
salismazaya <salismazaya@gmail.com>
2024-10-20 11:30:29 +0700

    delete front end for re-template

--- front-end/src/components/LoadingComponent.tsx
+++
@@ -1,3 +0,0 @@
-export default () => {
-    return <span className="loading loading-spinner loading-lg"></span>
-}
--- front-end/src/variables/index.ts
+++
@@ -1,8 +0,0 @@
-export const multiSigAddress = process.env.NEXT_PUBLIC_MULTISIG_ADDRESS as `0x${string}`;
-export const IdrtAddress = process.env.NEXT_PUBLIC_IDRT_ADDRESS as `0x${string}`;
-export const kasepAddress = process.env.NEXT_PUBLIC_KASEP_ADDRESS as `0x${string}`;
-// export const pusherKey = process.env.NEXT_PUBLIC_PUSHER_KEY as string;
-// export const pusherCluster = process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string;
-export const isTestnet = process.env.NEXT_PUBLIC_IS_TESTNET == 'true';
-export const wssRpcUrl = process.env.NEXT_PUBLIC_WSS_RPC as string;
-
