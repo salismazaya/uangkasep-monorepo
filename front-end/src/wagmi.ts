7bdde4538ecb0348112dc0fe701562ea825c1375
salismazaya <salismazaya@gmail.com>
2024-10-28 11:44:07 +0700

    ✨ (wagmi.ts): introduce wallet connectors for improved wallet integration ♻️ (wagmi.ts): refactor configuration to use createConfig instead of getDefaultConfig for better flexibility and maintainability

--- front-end/src/wagmi.ts
+++ front-end/src/wagmi.ts
@@ -5,12 +5,57 @@ import {
   sepolia
 } from 'wagmi/chains'
 
-import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+// import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import {
+  argentWallet,
+  coinbaseWallet,
+  ledgerWallet,
+  metaMaskWallet,
+  rabbyWallet,
+  rainbowWallet,
+  safeWallet,
+  walletConnectWallet,
+  braveWallet
+} from "@rainbow-me/rainbowkit/wallets";
+import { createConfig, http } from 'wagmi';
 
-const config = getDefaultConfig({
-  appName: process.env.NEXT_PUBLIC_WALLETCONNECT_APPNAME as string,
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended",
+      wallets: [
+        metaMaskWallet,
+        rainbowWallet,
+        walletConnectWallet,
+        ledgerWallet,
+        rabbyWallet,
+        coinbaseWallet,
+        argentWallet,
+        safeWallet,
+        braveWallet
+      ],
+    },
+  ],
+  { appName: process.env.NEXT_PUBLIC_WALLETCONNECT_APPNAME as string, projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string },
+);
+
+// const config = getDefaultConfig({
+//   appName: process.env.NEXT_PUBLIC_WALLETCONNECT_APPNAME as string,
+//   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
+//   chains: [isTestnet ? sepolia : polygon],
+// })
+
+const transports: Record<number, any> = {
+  [sepolia.id]: http(),
+  [polygon.id]: http(),
+};
+
+
+const config = createConfig({
   chains: [isTestnet ? sepolia : polygon],
+  connectors,
+  transports,
 })
 
 export default config
